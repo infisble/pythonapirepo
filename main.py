8686a4e0ed03e06f6f025feb70e4519109e2b9d1
Anton Vovk <143019553+infisble@users.noreply.github.com>
2025-03-31 10:04:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+app = FastAPI()
+
+class Message(BaseModel):
+    user_input: str
+
+@app.post("/chat")
+def chat(msg: Message):
+    url = "https://api.intelligence.io.solutions/api/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6ImI4OTczMjQzLTJiOGUtNGViMi05ZGI2LTQ2ZTAyZGM4Zjg2ZSIsImV4cCI6NDg5Njk3NjE4Nn0.LHeUGbvIvfnrunZup8r4a7Z8DmiP88hNeDZSBmq5oRUmyOnkaHLP4TcyIUIaJ7McM7ad21XEt2SgVfFU8T6zPw",
+    }
+    data = {
+        "model": "deepseek-ai/DeepSeek-R1",
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": msg.user_input}
+        ],
+    }
+    response = requests.post(url, headers=headers, json=data)
+    content = response.json()['choices'][0]['message']['content']
+    return {"response": content.split('</think>\n\n')[1]}
 
